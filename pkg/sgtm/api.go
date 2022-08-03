b0c1fe73408334281f4b07a7811eb19e817de2c4
Aftergrinder <110909617+Aftergrinder@users.noreply.github.com>
2022-08-03 21:31:56 +0000

    another tweak

--- pkg/sgtm/api.go
+++ pkg/sgtm/api.go
@@ -16,4 +16,13 @@ func (svc *Service) claimsFromContext(ctx context.Context) (*jwtClaims, error) {
 		return nil, fmt.Errorf("cannot get metadata from context")
 	}
 
-	oauthToken, ok := md["oa
+	oauthToken, ok := md["oauth-token"]
+	if !ok || len(oauthToken) == 0 {
+		return nil, fmt.Errorf("no such oauth-token")
+	}
+
+	return svc.parseJWTToken(oauthToken[0])
+}
+
+func (svc *Service) Me(ctx context.Context, req *sgtmpb.Me_Request) (*sgtmpb.Me_Response, error) {
+	claims, err := 
