50a7a177e0ce10c4b1f49ca7b69e44f996d59281
Aftergrinder <110909617+Aftergrinder@users.noreply.github.com>
2022-08-03 17:07:55 +0000

    Revert "Add localversion-wireless to identify builds from this tree."

--- pkg/sgtm/api.go
+++ pkg/sgtm/api.go
@@ -2,4 +2,18 @@ package sgtm
 
 import (
 	"context"
-	"fm
+	"fmt"
+	"os"
+	"time"
+
+	"google.golang.org/grpc/metadata"
+	"moul.io/sgtm/pkg/sgtmpb"
+)
+
+func (svc *Service) claimsFromContext(ctx context.Context) (*jwtClaims, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, fmt.Errorf("cannot get metadata from context")
+	}
+
+	oauthToken, ok := md["oa
