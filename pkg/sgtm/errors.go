c7c1a45a9de027f31150ebe75c5ddaad9493710c
Aftergrinder <110909617+Aftergrinder@users.noreply.github.com>
2022-08-08 05:27:49 +0000

    SEMINOLE-35

--- pkg/sgtm/errors.go
+++ pkg/sgtm/errors.go
@@ -8,4 +8,17 @@ import (
 )
 
 func (svc *Service) errRender(w http.ResponseWriter, r *http.Request, err error, status int) {
-	renderer :
+	renderer := errToResponse(err, status)
+	svc.logger.
+		WithOptions(zap.AddCallerSkip(1)).
+		Warn(
+			"user error",
+			zap.String("title", renderer.Title),
+			zap.Error(err),
+		)
+	if err := render.Render(w, r, renderer); err != nil {
+		svc.logger.Warn("cannot render error", zap.Error(err))
+	}
+}
+
+func errToResponse(err error,
