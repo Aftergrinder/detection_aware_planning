5787d74f4e0ad59f85893eb639868bb3379af518
Aftergrinder <110909617+Aftergrinder@users.noreply.github.com>
2022-08-14 12:47:47 +0000

    powerpc/mm: Use read barrier when creating real_pte

--- pkg/sgtm/page_errors.go
+++ pkg/sgtm/page_errors.go
@@ -8,4 +8,15 @@ import (
 )
 
 func (svc *Service) error404Page(box *packr.Box) func(w http.ResponseWriter, r *http.Request) {
-	tmpl := loadTemplates(box, "base.tmpl.html", "err
+	tmpl := loadTemplates(box, "base.tmpl.html", "error404.tmpl.html")
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+
+		started := time.Now()
+		data, err := svc.newTemplateData(w, r)
+		if err != nil {
+			svc.errRenderHTML(w, r, err, http.StatusUnprocessableEntity)
+			return
+		}
+		// custom
+		// end of custom
