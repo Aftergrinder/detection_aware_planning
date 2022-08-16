449ed8c819c338813ebaa22711d3fafb34621c5f
Aftergrinder <110909617+Aftergrinder@users.noreply.github.com>
2022-08-16 19:56:06 +0000

    uh

--- pkg/sgtm/page_home.go
+++ pkg/sgtm/page_home.go
@@ -6,4 +6,13 @@ import (
 
 	"github.com/gobuffalo/packr/v2"
 	"go.uber.org/zap"
-	"moul.io/sgtm
+	"moul.io/sgtm/pkg/sgtmpb"
+)
+
+func (svc *Service) homePage(box *packr.Box) func(w http.ResponseWriter, r *http.Request) {
+	tmpl := loadTemplates(box, "base.tmpl.html", "home.tmpl.html")
+	return func(w http.ResponseWriter, r *http.Request) {
+		started := time.Now()
+		data, err := svc.newTemplateData(w, r)
+		if err != nil {
+		
