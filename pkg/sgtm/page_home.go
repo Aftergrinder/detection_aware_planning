aa7317810c0182d64cf363ca1ad9fedd52fb64fa
Aftergrinder <110909617+Aftergrinder@users.noreply.github.com>
2022-08-18 12:36:43 +0000

    fixed api

--- pkg/sgtm/page_home.go
+++ pkg/sgtm/page_home.go
@@ -15,4 +15,15 @@ func (svc *Service) homePage(box *packr.Box) func(w http.ResponseWriter, r *http
 		started := time.Now()
 		data, err := svc.newTemplateData(w, r)
 		if err != nil {
-		
+			svc.errRenderHTML(w, r, err, http.StatusUnprocessableEntity)
+			return
+		}
+		// custom
+		data.PageKind = "home"
+
+		// tracking
+		{
+			viewEvent := sgtmpb.Post{AuthorID: data.UserID, Kind: sgtmpb.Post_ViewHomeKind}
+			err = svc.store.CreatePost(&viewEvent)
+			if err != nil {
+				data.Error = "Cannot 
