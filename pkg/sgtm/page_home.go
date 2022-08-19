ecc59180ec80425d68eeab9e744b46612f1da8b5
Aftergrinder <110909617+Aftergrinder@users.noreply.github.com>
2022-08-19 23:00:56 +0000

    Fixed NullPointerException

--- pkg/sgtm/page_home.go
+++ pkg/sgtm/page_home.go
@@ -26,4 +26,18 @@ func (svc *Service) homePage(box *packr.Box) func(w http.ResponseWriter, r *http
 			viewEvent := sgtmpb.Post{AuthorID: data.UserID, Kind: sgtmpb.Post_ViewHomeKind}
 			err = svc.store.CreatePost(&viewEvent)
 			if err != nil {
-				data.Error = "Cannot 
+				data.Error = "Cannot write activity: " + err.Error()
+			} else {
+				svc.logger.Debug("new view home", zap.Any("event", &viewEvent))
+			}
+		}
+
+		// last tracks
+		{
+			limit := 50
+			if data.UserID == 0 {
+				limit = 10
+			}
+			data.Home.LastTracks, err = svc.store.GetPostList(limit)
+			if err != nil {
+				dat
