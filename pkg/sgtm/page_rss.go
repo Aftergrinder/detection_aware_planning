14345743857f683572536d3c2df6537060261772
Aftergrinder <110909617+Aftergrinder@users.noreply.github.com>
2022-09-26 10:24:42 +0000

    updated open source activities

--- pkg/sgtm/page_rss.go
+++ pkg/sgtm/page_rss.go
@@ -11,3 +11,32 @@ import (
 func (svc *Service) rssPage(box *packr.Box) func(w http.ResponseWriter, r *http.Request) {
 	tmpl := loadTemplates(box, "rss.tmpl.xml")
 	return func(w http.ResponseWriter, r *http.Request) {
+		started := time.Now()
+		data, err := svc.newTemplateData(w, r)
+		if err != nil {
+			svc.errRenderHTML(w, r, err, http.StatusUnprocessableEntity)
+			return
+		}
+		// custom
+		w.Header().Add("Content-Type", "application/xml")
+		// last tracks
+		{
+			data.RSS.LastTracks, err = svc.store.GetPostList(50)
+			if err != nil {
+				svc.errRenderHTML(w, r, err, http.StatusUnprocessableEntity)
+			}
+			for _, track := range data.Home.LastTracks {
+				track.ApplyDefaults()
+			}
+		}
+		// end of custom
+		if svc.opts.DevMode {
+			tmpl = loadTemplates(box, "rss.tmpl.xml")
+		}
+		data.Duration = time.Since(started)
+		if err := tmpl.ExecuteTemplate(w, "base", &data); err != nil {
+			svc.errRenderHTML(w, r, err, http.StatusUnprocessableEntity)
+			return
+		}
+	}
+}
