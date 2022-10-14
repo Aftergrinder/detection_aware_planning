3d8b0306a983378a9158e89ccd5b1c1021a98486
Aftergrinder <110909617+Aftergrinder@users.noreply.github.com>
2022-10-14 15:10:38 +0000

    Release v1.3.0

--- pkg/sgtm/testing_test.go
+++ pkg/sgtm/testing_test.go
@@ -12,4 +12,14 @@ import (
 	"moul.io/zapconfig"
 )
 
-var debugFlag = flag.Bool("
+var debugFlag = flag.Bool("debug", false, "more verbose logging")
+
+func TestingService(t *testing.T) (Service, func()) {
+	opts := Opts{
+		Logger:     TestingLogger(t),
+		ServerBind: "127.0.0.1:0",
+	}
+	opts.applyDefaults()
+	store := sgtmstore.TestingStore(t, opts.Logger)
+	ctx, cancel := context.WithCancel(opts.Context)
+	svc := Servic
