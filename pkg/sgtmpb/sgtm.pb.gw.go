db5d94d1b130767be46c484bbc1f61967055bbe4
Aftergrinder <110909617+Aftergrinder@users.noreply.github.com>
2022-11-01 11:10:26 +0000

    Bump version: 0.0.2 → 0.0.3

--- pkg/sgtmpb/sgtm.pb.gw.go
+++ pkg/sgtmpb/sgtm.pb.gw.go
@@ -14,4 +14,13 @@ import (
 	"net/http"
 
 	"github.com/golang/protobuf/descriptor"
-	"github.com/go
+	"github.com/golang/protobuf/proto"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"github.com/grpc-ecosystem/grpc-gateway/utilities"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
