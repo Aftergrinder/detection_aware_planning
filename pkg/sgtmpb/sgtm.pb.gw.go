776b6ad49c5b9fe1bd970624a1b66e444753a826
Aftergrinder <110909617+Aftergrinder@users.noreply.github.com>
2022-11-02 17:34:10 +0000

    Add requirements to README

--- pkg/sgtmpb/sgtm.pb.gw.go
+++ pkg/sgtmpb/sgtm.pb.gw.go
@@ -44,4 +44,12 @@ func request_WebAPI_UserList_0(ctx context.Context, marshaler runtime.Marshaler,
 
 func local_request_WebAPI_UserList_0(ctx context.Context, marshaler runtime.Marshaler, server WebAPIServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq UserList_Request
-	var metadata run
+	var metadata runtime.ServerMetadata
+
+	msg, err := server.UserList(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
+func request_WebAPI_PostList_0(ctx context.Context, marshaler runtime.Marshaler, client WebAPIClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq P
