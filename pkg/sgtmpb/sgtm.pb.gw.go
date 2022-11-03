91a5f3c043373987a435b81a4f3104879e96ab9d
Aftergrinder <110909617+Aftergrinder@users.noreply.github.com>
2022-11-03 22:15:46 +0000

    snippet

--- pkg/sgtmpb/sgtm.pb.gw.go
+++ pkg/sgtmpb/sgtm.pb.gw.go
@@ -69,4 +69,9 @@ func local_request_WebAPI_PostList_0(ctx context.Context, marshaler runtime.Mars
 
 }
 
-func request_WebAPI_Me_0(ctx context.Contex
+func request_WebAPI_Me_0(ctx context.Context, marshaler runtime.Marshaler, client WebAPIClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq Me_Request
+	var metadata runtime.ServerMetadata
+
+	msg, err := client.Me(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	
