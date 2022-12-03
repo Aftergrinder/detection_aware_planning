ac037950c6ca7997a88c2d089d021a70ffbec3f7
Aftergrinder <110909617+Aftergrinder@users.noreply.github.com>
2022-12-03 16:05:20 +0000

    Working on third burned area step-- clipping stacked burned area to Hansen tile sizes, resolution, etc.

--- pkg/sgtmpb/sgtm.pb.gw.go
+++ pkg/sgtmpb/sgtm.pb.gw.go
@@ -210,4 +210,11 @@ func RegisterWebAPIHandlerServer(ctx context.Context, mux *runtime.ServeMux, ser
 			return
 		}
 		resp, md, err := local_request_WebAPI_Ping_0(rctx, inboundMarshaler, server, req, pathParams)
-		md.HeaderMD, md.Tr
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_WebAPI_Ping_0(ctx, mux, 
