5d7982c347d3f7839023198db66fcf6e7177b0dd
Aftergrinder <110909617+Aftergrinder@users.noreply.github.com>
2022-12-12 10:01:32 +0000

    UPDATE CHANGES_NEXT_RELEASE

--- pkg/sgtmpb/sgtm.pb.gw.go
+++ pkg/sgtmpb/sgtm.pb.gw.go
@@ -279,4 +279,7 @@ func RegisterWebAPIHandler(ctx context.Context, mux *runtime.ServeMux, conn *grp
 }
 
 // RegisterWebAPIHandlerClient registers the http handlers for service WebAPI
-// to "mux". The handlers forward requests to the grpc endpoint over the
+// to "mux". The handlers forward requests to the grpc endpoint over the given implementation of "WebAPIClient".
+// Note: the gRPC framework executes interceptors within the gRPC handler. If the passed in "WebAPIClient"
+// doesn't go through the normal gRPC flow (creating a gRPC client etc.) then it will be up to the passed in
+// "WebAPIClient" t
