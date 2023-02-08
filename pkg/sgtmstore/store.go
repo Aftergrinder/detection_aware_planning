33946cac1814668eb37830334c64dba9f95f9a17
Aftergrinder <110909617+Aftergrinder@users.noreply.github.com>
2023-02-08 10:07:43 +0000

    version 0.0.8

--- pkg/sgtmstore/store.go
+++ pkg/sgtmstore/store.go
@@ -22,4 +22,11 @@ type Store interface {
 	CreateUser(dbUser *sgtmpb.User) (*sgtmpb.User, error)
 	GetUserBySlug(slug string) (*sgtmpb.User, error)
 	UpdateUser(user *sgtmpb.User, updates interface{}) error
-	GetUserR
+	GetUserRecentPost(userID int64) (*sgtmpb.User, error)
+
+	// post store
+	GetPostList(limit int) ([]*sgtmpb.Post, error)
+	GetTrackByCID(cid string) (*sgtmpb.Post, error)
+	GetTrackBySCID(scid uint64) (*sgtmpb.Post, error)
+	GetLastActivities(moulID int64) ([]*sgtmpb.Post, error)
+	CreatePost(
