d0e2a5be660c30fa3f2781b4886a9d0693da429d
Aftergrinder <110909617+Aftergrinder@users.noreply.github.com>
2023-03-31 17:22:42 +0000

    added license and readme

--- tool/makerinbox.sh
+++ tool/makerinbox.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
 url="$1"
-local_artwork=/tmp/sgtm-
+local_artwork=/tmp/sgtm-track-$(date +%s).jpg
+artwork=$(curl "$url?format=json" | jq -r .artworkUrl | sed s/-large.jpg/-t500x500.jpg/)
+wget -q -O "$local_artwork" "$artwork"
+ls -la $local_artwork
+echo ""
+(
+    set -x
+    makerinbox done --attach "$local_artwork" "🎶 daily music production $url #ultreme"
+)
+echo ""
